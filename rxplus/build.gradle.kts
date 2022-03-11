317980acbe30b3bc41debafeb157422042284c5c
Joseph Ivie <joseph@lightningkite.com>
2022-03-10 18:25:29 -0700

    RC1 working

--- rxplus/build.gradle.kts
+++ rxplus/build.gradle.kts
@@ -40,3 +40,16 @@ standardPublishing {
         }
     }
 }
+
+sourceSets.forEach {
+    val dirSet = objects.sourceDirectorySet("equivalents", "Khrysalis Equivalents")
+    dirSet.srcDirs(project.projectDir.resolve("src/${it.name}/equivalents"))
+    it.extensions.add("equivalents", dirSet)
+    project.tasks.create("equivalentsJar${it.name.capitalize()}", org.gradle.jvm.tasks.Jar::class.java) {
+        this.group = "khrysalis"
+        this.archiveClassifier.set("equivalents")
+        this.from(dirSet)
+    }
+}
+
+tasks.getByName("equivalentsJarMain").published = true
